fe457a6ea0f1629f676ea3c491c83b041f3e8ac7
CIBoard <theciboard@gmail.com>
2018-11-19 16:36:20 +0900

    컨텐츠몰에서 찜한 목록이 삭제가 안되던 문제 해결

--- views/cmall/bootstrap/wishlist.php
+++ views/cmall/bootstrap/wishlist.php
@@ -12,7 +12,7 @@
 					<a href="<?php echo element('item_url', $result); ?>" title="<?php echo html_escape(element('cit_name', $result)); ?>"><img src="<?php echo thumb_url('cmallitem', element('cit_file_1', $result), 260, 260); ?>" alt="<?php echo html_escape(element('cit_name', $result)); ?>" title="<?php echo html_escape(element('cit_name', $result)); ?>" /></a>
 					<a href="<?php echo element('item_url', $result); ?>" title="<?php echo html_escape(element('cit_name', $result)); ?>" class="prd-tit"><?php echo html_escape(element('cit_name', $result)); ?></a>
 					<span class="prd-date"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo display_datetime(element('cwi_datetime', $result), 'full'); ?></span>
-					<button class="btn btn-xs" type="button" data-one-delete-url = "<?php echo element('delete_url', $result); ?>"><i class="fa fa-trash"></i> 삭제</button>
+					<button class="btn btn-xs btn-danger btn-one-delete" type="button" data-one-delete-url = "<?php echo element('delete_url', $result); ?>"><i class="fa fa-trash"></i> 삭제</button>
 				</li>
 			<?php
 				}
